7fe93c7632da125a57fe37264b0d7db524274e59
OrlowskiDaniel <142877619+OrlowskiDaniel@users.noreply.github.com>
2026-04-08 21:13:43 +0200

    player class

---
+++ Blackjack-course/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace Blackjack_course
+{
+    public class Player
+    {
+        // fields
+        
+        public int Chips { get; private set; }
+        public bool AllowedPlay { get; set; }
+
+        // constructor
+        public Player(int chips)
+        {
+            Chips = chips;
+            
+            AllowedPlay = true;
+        }
+
+        // add a card to the current hand
+       
+        // reset player for new round
+        public void Reset()
+        {
+            Hands.Clear();
+            AllowedPlay = true;
+        }
+    }
+}
